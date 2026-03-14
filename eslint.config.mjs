cc86ce3687f75d9b7e49affcf1a5b5c094f2d7a2
Oscar Wellner <oscar.wellner@gmail.com>
2026-03-14 20:38:14 +0100

    Chore: Run prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,26 +1,26 @@
 import eslint from '@eslint/js';
+import { defineConfig, globalIgnores } from 'eslint/config';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
-import { globalIgnores, defineConfig } from 'eslint/config';
 
 export default defineConfig(
-  globalIgnores(['eslint.config.mjs']),
-  eslint.configs.recommended,
-  tseslint.configs.recommendedTypeChecked,
-  tseslint.configs.stylisticTypeChecked,
-  {
-    languageOptions: {
-      globals: {
-        ...globals.node,
-      },
-      sourceType: 'commonjs',
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
+    globalIgnores(['eslint.config.mjs']),
+    eslint.configs.recommended,
+    tseslint.configs.recommendedTypeChecked,
+    tseslint.configs.stylisticTypeChecked,
+    {
+        languageOptions: {
+            globals: {
+                ...globals.node,
+            },
+            sourceType: 'commonjs',
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
     },
-  },
-  {
-    rules: {},
-  },
+    {
+        rules: {},
+    }
 );
