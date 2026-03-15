26c80351cf30dd5cd661d5231d7f8253fe888bf0
Oscar Wellner <oscar.wellner@gmail.com>
2026-03-15 15:04:18 +0100

    Feat: Configure and use throttler

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { provideAppThrottler, throttlerModuleOptions } from './config';
 import { HealthModule } from './health/health.module';
 
 @Module({
-    imports: [HealthModule],
+    imports: [ThrottlerModule.forRoot(throttlerModuleOptions), HealthModule],
+    providers: [provideAppThrottler()],
 })
 export class AppModule {}
