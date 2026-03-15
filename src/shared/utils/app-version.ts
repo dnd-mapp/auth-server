1e55b62c7cd54e19245e76ca458435ac3112a426
Oscar Wellner <oscar.wellner@gmail.com>
2026-03-15 21:10:03 +0100

    Chore: Provide return type

--- src/shared/utils/app-version.ts
+++ src/shared/utils/app-version.ts
@@ -6,8 +6,8 @@ export async function appVersion() {
     if (version) return version;
 
     const packageManifestContents = await readFile('package.json', { encoding: 'utf8' });
-    const packageManifest = JSON.parse(packageManifestContents);
+    const packageManifest = JSON.parse(packageManifestContents) as { version: string };
 
-    version = packageManifest.version as string;
+    version = packageManifest.version;
     return version;
 }
