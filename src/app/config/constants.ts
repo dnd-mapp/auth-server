914750fd22b4844badfa96cc54b4f16e80b2e792
Oscar Wellner <oscar.wellner@gmail.com>
2026-03-15 23:13:30 +0100

    Feat: Add constants for min and max port values

--- src/app/config/constants.ts
+++ src/app/config/constants.ts
@@ -2,6 +2,10 @@ import { convertTime, TimeUnits } from '@/shared-utils';
 
 export const DEFAULT_SERVER_PORT = 4350;
 
+export const PORT_RANGE_MIN = 1024;
+
+export const PORT_RANGE_MAX = 65535;
+
 export const DEFAULT_SERVER_HOST = '0.0.0.0';
 
 export const DEFAULT_CORS_ORIGINS = ['https://localhost.www.dndmapp.dev:4200'];
