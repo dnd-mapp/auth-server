c2144e9c4294041380b3279af2dc5cf16cb1e6da
Oscar Wellner <oscar.wellner@gmail.com>
2026-03-15 10:13:38 +0100

    Feat: Change default port and use constant variable

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const DEFAULT_SERVER_PORT = 4350;
+
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    await app.listen(process.env.PORT ?? 3000);
+    await app.listen(process.env['PORT'] ?? DEFAULT_SERVER_PORT);
 }
 
 bootstrap().catch((error) => console.error(error));
