e3f05936d9a099b9734b8869f7a4d074f213ced6
Oscar Wellner <oscar.wellner@gmail.com>
2026-03-15 23:12:34 +0100

    Feat: Change default host for containerized apps

--- src/app/config/constants.ts
+++ src/app/config/constants.ts
@@ -2,7 +2,7 @@ import { convertTime, TimeUnits } from '@/shared-utils';
 
 export const DEFAULT_SERVER_PORT = 4350;
 
-export const DEFAULT_SERVER_HOST = 'localhost';
+export const DEFAULT_SERVER_HOST = '0.0.0.0';
 
 export const DEFAULT_CORS_ORIGINS = ['https://localhost.www.dndmapp.dev:4200'];
 
