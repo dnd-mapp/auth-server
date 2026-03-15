2ed2e066895ca57dd3b9413c20f4d1c843d86c99
Oscar Wellner <oscar.wellner@gmail.com>
2026-03-15 23:16:00 +0100

    Feat: Configure and enable Nestjs Configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { ThrottlerModule } from '@nestjs/throttler';
-import { provideAppThrottler, throttlerModuleOptions } from './config';
+import { configModuleOptions, provideAppThrottler, throttlerModuleOptions } from './config';
 import { HealthModule } from './health/health.module';
 
 @Module({
-    imports: [ThrottlerModule.forRoot(throttlerModuleOptions), HealthModule],
+    imports: [ConfigModule.forRoot(configModuleOptions), ThrottlerModule.forRoot(throttlerModuleOptions), HealthModule],
     providers: [provideAppThrottler()],
 })
 export class AppModule {}
