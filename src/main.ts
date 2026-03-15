beabcd62dc26e77b62f0c6d68fd6cecf0220adaa
Oscar Wellner <oscar.wellner@gmail.com>
2026-03-15 12:05:08 +0100

    Feat: Use fastify instead of express

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { FastifyAdapter } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
 const DEFAULT_SERVER_PORT = 4350;
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    await app.listen(process.env['PORT'] ?? DEFAULT_SERVER_PORT);
+    const fastifyAdapter = new FastifyAdapter();
+
+    const app = await NestFactory.create(AppModule, fastifyAdapter);
+
+    const port = process.env['PORT'] ?? DEFAULT_SERVER_PORT;
+
+    await app.listen(port);
 }
 
 bootstrap().catch((error) => console.error(error));
