8e021366d134666a19aec3dc48869251a0de3820
Oscar Wellner <oscar.wellner@gmail.com>
2026-03-17 00:49:44 +0100

    Feat: Add database config namespace

--- src/app/config/configurations/configuration-namespaces.ts
+++ src/app/config/configurations/configuration-namespaces.ts
@@ -1,5 +1,6 @@
 export const ConfigurationNamespaces = {
     SERVER: 'server',
+    DATABASE: 'database',
 } as const;
 
 export type ConfigurationNamespace = (typeof ConfigurationNamespaces)[keyof typeof ConfigurationNamespaces];
