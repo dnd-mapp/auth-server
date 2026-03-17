72cb76cd977f838fa4357bf2aec2b703cfe5647d
Oscar Wellner <oscar.wellner@gmail.com>
2026-03-17 11:07:18 +0100

    Feat: Include the databaseConfig

--- src/app/config/nestjs-config.ts
+++ src/app/config/nestjs-config.ts
@@ -1,10 +1,10 @@
 import { ConfigModuleOptions } from '@nestjs/config';
-import { serverConfig } from './configurations';
+import { databaseConfig, serverConfig } from './configurations';
 import { validateEnvironmentVariables } from './validation/validate-environment-variables';
 
 export const configModuleOptions: ConfigModuleOptions = {
     envFilePath: ['.env'],
     expandVariables: true,
-    load: [serverConfig],
+    load: [serverConfig, databaseConfig],
     validate: validateEnvironmentVariables,
 };
