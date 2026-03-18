a43cfe0089585c8bb23d055698d622db9be05773
Oscar Wellner <oscar.wellner@gmail.com>
2026-03-18 21:56:49 +0100

    Chore: Add mocks for retrieving a single user by ID

--- src/auth/domain/test/mock-user.db.ts
+++ src/auth/domain/test/mock-user.db.ts
@@ -1,9 +1,19 @@
-import { User } from '@/auth-domain';
+import { User, UserBuilder } from '@/auth-domain';
+
+export const theLegend27 = new UserBuilder().withId('mb9NzZCnMCCrWoETc2_DT').withUsername('TheLegend27').build();
 
 export class MockUserDB {
-    private users: Record<string, User> = {};
+    private users: Record<string, User>;
+
+    constructor() {
+        this.users = { [theLegend27.id]: theLegend27 };
+    }
 
     public getAll(): User[] {
         return Object.values(this.users);
+    }
+
+    public getById(id: string) {
+        return this.users[id] ?? null;
     }
 }
