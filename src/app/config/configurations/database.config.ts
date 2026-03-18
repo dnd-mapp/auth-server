3c924023e454274f930165c50268799adb36683d
Oscar Wellner <oscar.wellner@gmail.com>
2026-03-18 12:08:23 +0100

    Feat: Make function synchronous

--- src/app/config/configurations/database.config.ts
+++ src/app/config/configurations/database.config.ts
@@ -4,7 +4,7 @@ import { registerAs } from '@nestjs/config';
 import { DEFAULT_DB_HOST, DEFAULT_DB_PORT, DEFAULT_DB_SCHEMA, DEFAULT_DB_USER } from '../constants';
 import { EnvironmentVariableNames } from '../validation/environment-variables.schema';
 
-export const databaseConfig = registerAs<DatabaseConfig>(ConfigurationNamespaces.DATABASE, async () => {
+export const databaseConfig = registerAs<DatabaseConfig>(ConfigurationNamespaces.DATABASE, () => {
     return {
         host: process.env[EnvironmentVariableNames.DB_HOST] ?? DEFAULT_DB_HOST,
         port: parseInteger(DEFAULT_DB_PORT, process.env[EnvironmentVariableNames.DB_PORT]),
