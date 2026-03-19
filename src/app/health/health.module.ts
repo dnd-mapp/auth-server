15fd7f61ebe01b8d2c793eb5114922aab3f93d4a
Oscar Wellner <oscar.wellner@gmail.com>
2026-03-19 15:50:18 +0100

    Feat: Add database check into the readiness check

--- src/app/health/health.module.ts
+++ src/app/health/health.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TerminusModule } from '@nestjs/terminus';
+import { DatabaseModule } from '../database';
 import { HealthController } from './health.controller';
 
 @Module({
-    imports: [TerminusModule],
+    imports: [TerminusModule, DatabaseModule],
     controllers: [HealthController],
 })
 export class HealthModule {}
