50b8028efef97b63a379f2ebd7529844f515bc79
Oscar Wellner <oscar.wellner@gmail.com>
2026-03-19 14:42:48 +0100

    Feat: Add end-point to permanently remove a User

--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, Logger, NotFoundException } from '@nestjs/common';
+import { GetUserQueryParams } from './dtos';
 import { UserRepository } from './user.repository';
 
 @Injectable()
@@ -14,17 +15,27 @@ export class UserService {
         return await this.userRepository.findAll();
     }
 
-    public async getById(id: string) {
-        return await this.userRepository.findById(id);
+    public async getById(id: string, params?: GetUserQueryParams) {
+        return await this.userRepository.findById(id, params);
     }
 
     public async removeById(id: string) {
         const byId = await this.getById(id);
 
         if (byId == null) {
-            this.logger.warn(`Failed to delete user: No user found with ID "${id}"`);
+            this.logger.warn(`Failed to soft delete user: No user found with ID "${id}"`);
             throw new NotFoundException(`User with ID "${id}" not found`);
         }
         await this.userRepository.softDeleteById(id);
+    }
+
+    public async purgeById(id: string) {
+        const byId = await this.getById(id, { includeDeactivated: true });
+
+        if (byId == null) {
+            this.logger.warn(`Failed to purge user: No user found with ID "${id}"`);
+            throw new NotFoundException(`User with ID "${id}" not found`);
+        }
+        await this.userRepository.purgeById(id);
     }
 }
