01eaa1f7c0877ec077fe256d3136be77936dc46d
Oscar Wellner <oscar.wellner@dndmapp.nl.eu.org>
2026-04-24 17:41:51 +0200

    feat(client): update DTOs to expose clientType and accept clientSecret

--- src/client/dtos/client.dto.ts
+++ src/client/dtos/client.dto.ts
@@ -1,5 +1,6 @@
 import { Client } from '@/client/domain';
-import { IsArray, IsDate, IsNotEmpty, IsString, IsUrl, MinLength } from 'class-validator';
+import { IsArray, IsDate, IsEnum, IsNotEmpty, IsString, IsUrl, MinLength } from 'class-validator';
+import { ClientType, ClientTypes } from '../client.constants';
 
 export class ClientDto implements Client {
     /**
@@ -18,6 +19,13 @@ export class ClientDto implements Client {
     @IsNotEmpty()
     @IsString()
     public name!: string;
+
+    /**
+     * The OAuth 2.0 client type.
+     * @example 'public'
+     */
+    @IsEnum(ClientTypes)
+    public clientType!: ClientType;
 
     /**
      * The list of allowed redirect URIs for the client.
--- src/client/dtos/update-client.dto.ts
+++ src/client/dtos/update-client.dto.ts
@@ -1,7 +1,19 @@
 import { UpdateClientData } from '@/client/domain';
 import { PickType } from '@nestjs/swagger';
+import { IsEnum, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { ClientType, ClientTypes } from '../client.constants';
 import { ClientDto } from './client.dto';
 
 export class UpdateClientDto
     extends PickType(ClientDto, ['name', 'allowedUris'] as const)
-    implements UpdateClientData {}
+    implements UpdateClientData
+{
+    @IsEnum(ClientTypes)
+    @IsOptional()
+    public clientType: ClientType = ClientTypes.PUBLIC;
+
+    @IsNotEmpty()
+    @IsString()
+    @IsOptional()
+    public clientSecret?: string;
+}
