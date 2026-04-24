5cc25042ffd59811accadab5a0288bc55d53421b
Oscar Wellner <oscar.wellner@dndmapp.nl.eu.org>
2026-04-24 17:41:41 +0200

    feat(client): add ClientTypes constant and extend domain model

--- src/client/domain/client.model.ts
+++ src/client/domain/client.model.ts
@@ -1,10 +1,13 @@
+import { ClientType } from '../client.constants';
+
 export interface Client {
     id: string;
     name: string;
+    clientType: ClientType;
     allowedUris: string[];
     createdAt: Date;
     updatedAt: Date;
 }
 
-export type CreateClientData = Pick<Client, 'name' | 'allowedUris'>;
-export type UpdateClientData = Pick<Client, 'name' | 'allowedUris'>;
+export type CreateClientData = Pick<Client, 'name' | 'allowedUris' | 'clientType'>;
+export type UpdateClientData = Pick<Client, 'name' | 'allowedUris' | 'clientType'>;
