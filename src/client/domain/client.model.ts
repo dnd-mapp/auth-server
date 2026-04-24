3c30a389398fa839968e73301da0442bc3564397
Oscar Wellner <oscar.wellner@dndmapp.nl.eu.org>
2026-04-24 22:33:14 +0200

    refactor(client): restructure domain — rename constants to enum and move builder into domain folder

--- src/client/domain/client.model.ts
+++ src/client/domain/client.model.ts
@@ -1,13 +1,15 @@
-import { ClientType } from '../client.constants';
+import { ClientType } from './client-type.enum';
 
 export interface Client {
     id: string;
     name: string;
     clientType: ClientType;
+    clientSecret: string | null;
     allowedUris: string[];
     createdAt: Date;
     updatedAt: Date;
 }
 
-export type CreateClientData = Pick<Client, 'name' | 'allowedUris' | 'clientType'>;
-export type UpdateClientData = Pick<Client, 'name' | 'allowedUris' | 'clientType'>;
+export type CreateClientData = Pick<Client, 'name' | 'allowedUris' | 'clientType' | 'clientSecret'>;
+
+export type UpdateClientData = Pick<Client, 'name' | 'allowedUris' | 'clientType' | 'clientSecret'>;
